a84728ca4df652835d6eb620abb709ad60e5dd6d
Pablo Collado <pablo.collado@borealos.com>
2017-09-21 12:03:44 +0200

    ajustando reloj

--- js/js_timer2.js
+++ js/js_timer2.js
@@ -23,6 +23,7 @@ function iniciar() {
 
 
     function muestraEstado(dato) { // muestra en pantalla estado crono: trabajando, descansando o parado.
+        document.getElementById("estadoCrono").style.textAlign = "center";
         document.getElementById("estadoCrono").innerHTML = dato;
     }
 
@@ -46,7 +47,7 @@ function iniciar() {
         document.getElementById("minutos").innerHTML = datoMin;
 
 
-        if (!stop) x = setTimeout(function() { crono(datoSeg, datoMin) }, 500);
+        if (!stop) x = setTimeout(function() { crono(datoSeg, datoMin) }, 1000);
     }
 
     function controlador(datoSeg, datoMin) {
@@ -61,11 +62,11 @@ function iniciar() {
                     segundos = 59;
                     timer_is_on = 0;
                     estado = false;
-                    estadoCrono = "Ciclo " + (ciclo + 1) + " - Fin trabajo.</br>Play para iniciar descanso";
+                    estadoCrono = "Ciclo " + (ciclo + 1) + " - Fin trabajo. Play para iniciar descanso";
                     muestraEstado(estadoCrono);
-                    document.getElementById("btnInicio").style.border = "2px solid green";
-                    document.getElementById("btnPausa").style.border = "2px solid green";
-                    document.getElementById("btnStop").style.border = "4px solid green";
+                    document.getElementById("btnInicio").style.border = "3px solid #A0906F";
+                    document.getElementById("btnPausa").style.border = "3px solid #A0906F";
+                    document.getElementById("btnStop").style.border = "1px solid #A0906F";
                     alarma();
                 }
 
@@ -79,11 +80,11 @@ function iniciar() {
                     timer_is_on = 0;
                     minutos = wrkTime;
                     segundos = 59;
-                    estadoCrono = "Ciclo " + (ciclo) + " finalizado.<br> Play para iniciar ciclo:" + (ciclo + 1);
+                    estadoCrono = "Ciclo " + (ciclo) + " finalizado. Play para iniciar ciclo:" + (ciclo + 1);
                     muestraEstado(estadoCrono);
-                    document.getElementById("btnInicio").style.border = "2px solid green";
-                    document.getElementById("btnPausa").style.border = "2px solid green";
-                    document.getElementById("btnStop").style.border = "4px solid green";
+                    document.getElementById("btnInicio").style.border = "3px solid #A0906F";
+                    document.getElementById("btnPausa").style.border = "3px solid #A0906F";
+                    document.getElementById("btnStop").style.border = "1px solid #A0906F";
 
                     alarma();
                 }
@@ -104,9 +105,9 @@ function iniciar() {
                     estadoCrono = "Ciclo: " + (ciclo + 1) + " - Descansando";
                     minutos = wrkPause;
                 }
-                document.getElementById("btnInicio").style.border = "3px solid green";
-                document.getElementById("btnPausa").style.border = "2px solid green";
-                document.getElementById("btnStop").style.border = "2px solid green";
+                document.getElementById("btnInicio").style.border = "1px solid #A0906F";
+                document.getElementById("btnPausa").style.border = "3px solid #A0906F";
+                document.getElementById("btnStop").style.border = "3px solid #A0906F";
                 muestraEstado(estadoCrono);
                 prePintaCiclo(ciclo);
                 indicaCicloTxt(ciclo);
@@ -123,9 +124,9 @@ function iniciar() {
             clearTimeout(x);
             timer_is_on = 0;
             estadoCrono = "<b> En Pausa</b>";
-            document.getElementById("btnInicio").style.border = "2px solid green";
-            document.getElementById("btnPausa").style.border = "3px solid green";
-            document.getElementById("btnStop").style.border = "2px solid green";
+            document.getElementById("btnInicio").style.border = "3px solid #A0906F";
+            document.getElementById("btnPausa").style.border = "1px solid #A0906F";
+            document.getElementById("btnStop").style.border = "3px solid #A0906F";
             muestraEstado(estadoCrono);
             estadoPlay = true;
             estadoPause = false;
@@ -139,6 +140,7 @@ function iniciar() {
             estadoPause = false;
             estadoStop = false;
             iniciaPantalla();
+            indicaCicloTxt("");
         }
     });
 
@@ -161,23 +163,23 @@ function iniciar() {
         if ((dato != 0) && (dato <= 4)) {
             switch (dato) {
                 case 1:
-                    document.getElementById("barraEstado1").style.background = "green";
+                    document.getElementById("barraEstado1").style.border = "3px solid #A0906F";
                     break;
                 case 2:
-                    document.getElementById("barraEstado2").style.background = "green";
+                    document.getElementById("barraEstado2").style.border = "3px solid #A0906F";
                     break;
                 case 3:
-                    document.getElementById("barraEstado3").style.background = "green";
+                    document.getElementById("barraEstado3").style.border = "3px solid #A0906F";
                     break;
                 case 4:
-                    document.getElementById("barraEstado4").style.background = "green";
+                    document.getElementById("barraEstado4").style.border = "3px solid #A0906F";
                     break;
             };
         } else {
-            document.getElementById("barraEstado1").style.background = "transparent";
-            document.getElementById("barraEstado2").style.background = "transparent";
-            document.getElementById("barraEstado3").style.background = "transparent";
-            document.getElementById("barraEstado4").style.background = "transparent";
+            document.getElementById("barraEstado1").style.backgroundImage = "../img/favicon.png";
+            document.getElementById("barraEstado2").style.backgroundImage = "../img/favicon.png";
+            document.getElementById("barraEstado3").style.backgroundImage = "../img/favicon.png";
+            document.getElementById("barraEstado4").style.backgroundImage = "../img/favicon.png";
         }
     }
 
@@ -187,23 +189,23 @@ function iniciar() {
         if (dato < 4) {
             switch (dato) {
                 case 0:
-                    document.getElementById("barraEstado1").style.border = "2px solid green";
+                    document.getElementById("barraEstado1").style.border = "3px solid #A0906F";
                     break;
                 case 1:
-                    document.getElementById("barraEstado2").style.border = "2px solid green";
+                    document.getElementById("barraEstado2").style.border = "3px solid #A0906F";
                     break;
                 case 2:
-                    document.getElementById("barraEstado3").style.border = "2px solid green";
+                    document.getElementById("barraEstado3").style.border = "3px solid #A0906F";
                     break;
                 case 3:
-                    document.getElementById("barraEstado4").style.border = "2px solid green";
+                    document.getElementById("barraEstado4").style.border = "3px solid #A0906F";
                     break;
             };
         } else {
-            document.getElementById("barraEstado1").style.border = "1px solid green";
-            document.getElementById("barraEstado2").style.border = "1px solid green";
-            document.getElementById("barraEstado3").style.border = "1px solid green";
-            document.getElementById("barraEstado4").style.border = "1px solid green";
+            document.getElementById("barraEstado1").style.backgroundImage = "../img/favicon.png";
+            document.getElementById("barraEstado2").style.backgroundImage = "../img/favicon.png";
+            document.getElementById("barraEstado3").style.backgroundImage = "../img/favicon.png";
+            document.getElementById("barraEstado4").style.backgroundImage = "../img/favicon.png";
         }
     }
 
@@ -226,33 +228,32 @@ function iniciar() {
         var estadoStop = false;
         document.getElementById("minutos").innerHTML = "00";
         document.getElementById("segundos").innerHTML = "00";
-        document.getElementById("btnInicio").style.border = "2px solid #A0906F";
-        document.getElementById("btnPausa").style.border = "2px solid #A0906F";
-        document.getElementById("btnStop").style.border = "2px solid #A0906F";
-        document.getElementById("barraEstado1").style.background = "#198A19";
-        document.getElementById("barraEstado2").style.background = "#198A19";
-        document.getElementById("barraEstado3").style.background = "#198A19";
-        document.getElementById("barraEstado4").style.background = "#198A19";
-        document.getElementById("barraEstado1").style.border = "1px solid green";
-        document.getElementById("barraEstado2").style.border = "1px solid green";
-        document.getElementById("barraEstado3").style.border = "1px solid green";
-        document.getElementById("barraEstado4").style.border = "1px solid green";
+        document.getElementById("btnInicio").style.border = "3px solid #A0906F";
+        document.getElementById("btnPausa").style.border = "3px solid #A0906F";
+        document.getElementById("btnStop").style.border = "1px solid #A0906F";
+        document.getElementById("barraEstado1").style.backgroundColor = "rgba(52, 42, 33, 1)";
+        document.getElementById("barraEstado2").style.backgroundImage = "../img/favicon.png";
+        document.getElementById("barraEstado3").style.backgroundImage = "../img/favicon.png";
+        document.getElementById("barraEstado4").style.backgroundImage = "../img/favicon.png";
+        document.getElementById("barraEstado1").style.border = "1px solid #A0906F";
+        document.getElementById("barraEstado2").style.border = "1px solid #A0906F";
+        document.getElementById("barraEstado3").style.border = "1px solid #A0906F";
+        document.getElementById("barraEstado4").style.border = "1px solid #A0906F";
         pintaCiclo(0);
-
     }
 
     function finCuatroCiclos() {
-        estadoCrono = "Has finalizado los 4 ciclos!<br>Ahora se debe descansar unos 20 o 30 min<br>antes de iniciar de nuevo el ciclo de 4 pomodoros.";
+        estadoCrono = "Has finalizado los 4 ciclos!";
         muestraEstado(estadoCrono);
         clearTimeout(x);
         timer_is_on = 0;
         stop = true;
         document.getElementById("minutos").innerHTML = "00";
         document.getElementById("segundos").innerHTML = ":00";
-        document.getElementById("btnInicio").style.border = "2px solid green";
-        document.getElementById("btnPausa").style.border = "2px solid green";
-        document.getElementById("btnStop").style.border = "3px solid green";
-        document.getElementById("barraEstado4").style.background = "green";
+        document.getElementById("btnInicio").style.border = "1px solid #A0906F";
+        document.getElementById("btnPausa").style.border = "1px solid #A0906F";
+        document.getElementById("btnStop").style.border = "3px solid #A0906F";
+        document.getElementById("barraEstado4").style.backgroundImage = "../img/favicon.png";
         ciclo = 4;
         pintaCiclo(ciclo);
         alarma();
@@ -260,16 +261,16 @@ function iniciar() {
     }
 
     function alarma() {
-        document.getElementById("alarma").style.display = "block";
+        document.getElementById("alarma").style.visibility = "visible";
         estadoPlay = true;
         estadoPause = false;
         estadoStop = true;
-        var audio = document.getElementById("audio2");
+        var audio = document.getElementById("audio1");
         audio.loop = true;
         audio.volume = 1;
         audio.play();
         document.getElementById("btnAlarma").addEventListener("click", function pararAlarma() {
-            document.getElementById("alarma").style.display = "none";
+            document.getElementById("alarma").style.visibility = "hidden";
             audio.volume = 0;
             audio.loop = false;
         });
@@ -277,7 +278,8 @@ function iniciar() {
 
     function indicaCicloTxt(dato) {
         var x = document.getElementById("cicloPomodoro");
-        x.innerHTML = "Ciclo " + (ciclo + 1) + " de 4 Pomodoros"
+        x.innerHTML = "Ciclo " + (ciclo + 1) + " de 4 Pomodoros";
+        console.log("dentro de indicaciclo ok: " + ciclo);
     }
 
 
@@ -304,19 +306,4 @@ function iniciar() {
             });
         }
     }
-
-    document.getElementById("btnFondo").addEventListener("click", function cambiaFondo() {
-        if (fondo == "negro") {
-            document.body.style.backgroundColor = "white";
-            document.getElementById("btnFondo").style.backgroundColor = "white";
-            fondo = "blanco";
-
-        } else {
-            document.body.style.backgroundColor = "black";
-            fondo = "negro";
-            document.getElementById("btnFondo").style.backgroundColor = "black";
-        }
-    });
-
-
 } // fin corchete funcion iniciar
