93f8c721ff2998f3cfb935255ed94655c5cc553d
Pablo Collado <pablo.collado@borealos.com>
2021-10-04 18:51:48 +0200

    Modified min working time slider to 10

--- js/js_timer2.js
+++ js/js_timer2.js
@@ -107,7 +107,7 @@ function crono(datoSeg, datoMin) {
   if (!stop)
     x = setTimeout(function () {
       crono(datoSeg, datoMin);
-    }, 1);
+    }, 1000);
 }
 
 // Cuando crono llega a cero tanto en tiempo de trabajo como de descanso
